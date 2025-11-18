daaca21f377b6d335ab017b3806e73eb6ad749e7
sriharaz <sriharaz@amazon.com>
2025-11-19 03:33:00 +0530

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    'vuetify_hook',
+    '@nuxt/eslint'
+  ],
+  nitro: {
+    preset: 'aws-amplify',
+    logLevel: 5,
+    awsAmplify: {
+      // Set to true so Lambda doesn't try to serve static files from filesystem
+      catchAllStaticFallback: true
+    },
+    routeRules: {
+      '/api/**': { cors: true },
+      '/': { ssr: true },
+      '/static-test': { prerender: true }
+    }
+  },
+  css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  runtimeConfig: {
+    public: {}
+  }
 })
