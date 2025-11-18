5dc16eb5f6d35f4d4526339f93417fb634f8b34a
sriharaz <sriharaz@amazon.com>
2025-11-19 03:04:59 +0530

    My changes

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Nuxt Amplify Hosting Issue Reproduction</h1>
+    <p>This is the home page (SSR)</p>
+    <NuxtLink to="/static-test/">Go to Static Test Page</NuxtLink>
+    <NuxtLink to="/static-test">No Slash</NuxtLink>
+  </div>
+</template>
---
+++ pages/static-test/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Static Test Page</h1>
+    <p>This page should be prerendered as static HTML.</p>
+    <p>If you can see this after refreshing the page at /static-test, then the issue is resolved.</p>
+    <p>If you get a 502 error when refreshing at /static-test, then the issue is reproduced.</p>
+    <NuxtLink to="/">Back to Home</NuxtLink>
+  </div>
+</template>
