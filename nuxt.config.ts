950075ba60f45a35b52f4a320aed0d5daa9cef59
sriharaz <sriharaz@amazon.com>
2025-11-19 03:44:09 +0530

    modifying the packages as per supported versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/vuetify',
     '@nuxt/eslint'
   ],
   nitro: {
---
+++ plugins/vuetify.ts
@@ -0,0 +1,11 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  app.vueApp.use(vuetify)
+})
