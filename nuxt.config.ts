9ca29e5764ddaceb86c3f734f3203377b062f161
sriharaz <sriharaz@amazon.com>
2025-11-19 19:50:34 +0530

    reverting the code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,22 @@
-// export default defineNuxtConfig({
-//   compatibilityDate: '2025-07-15',
-//   devtools: { enabled: true },
-//   ssr: false,
-//   modules: [
-//     '@nuxt/eslint'
-//   ],
-//   nitro: {
-//     preset: 'aws-amplify',
-//     logLevel: 5,
-//     awsAmplify: {
-//       // Set to true so Lambda doesn't try to serve static files from filesystem
-//       catchAllStaticFallback: true
-//     },
-//     routeRules: {
-//       '/api/**': { cors: true },
-//       '/': { ssr: false },
-//       '/static-test': { prerender: true }
-//     }
-//   },
-//   css: [
-//     'vuetify/styles',
-//     '@mdi/font/css/materialdesignicons.css'
-//   ],
-//   build: {
-//     transpile: ['vuetify']
-//   },
-//   runtimeConfig: {
-//     public: {}
-//   }
-// })
-
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,  // ← Disable SSR (SPA mode)
+  ssr: false,
   modules: [
     '@nuxt/eslint'
   ],
-  // Remove or simplify nitro config
   nitro: {
-    preset: 'static',  // ← Use static preset
+    preset: 'aws-amplify',
+    logLevel: 5,
+    awsAmplify: {
+      // Set to true so Lambda doesn't try to serve static files from filesystem
+      catchAllStaticFallback: true
+    },
+    routeRules: {
+      '/api/**': { cors: true },
+      '/': { ssr: false },
+      '/static-test': { prerender: true }
+    }
   },
   css: [
     'vuetify/styles',
